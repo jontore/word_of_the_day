e1a22dc1f7e6c602ec7470ed176c96aeea0a3540
Jon Tore Hafstad <jontore@gmail.com>
2015-04-24 12:39:18 +0200

    Connected with esp8266

--- word_of_the_day/word_of_the_day.ino
+++ word_of_the_day/word_of_the_day.ino
@@ -1,5 +1,9 @@
+#include <SoftwareSerial.h>
+#include <espduino.h>
+#include <rest.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+
 
 #define I2C_ADDR      0x27 // I2C address of PCF8574A
 #define BACKLIGHT_PIN 3
@@ -13,16 +17,95 @@
 
 LiquidCrystal_I2C lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin, BACKLIGHT_PIN, POSITIVE);
 
-void setup()
+#define DEBUG
+
+#define SSID "babbel-hackday"
+#define PASSWORD "hackday_2015"
+
+#define DOMAIN "freegeoip.net"
+#define PATH "/json/"
+#define COUNTRY_CODE "CH"
+#ifdef DEBUG
+  SoftwareSerial debugPort(2, 3); // RX, TX
+#endif
+
+#ifdef DEBUG
+ESP esp(&Serial, &debugPort, 4);
+#else
+ESP esp(&Serial, 4);
+#endif
+
+REST rest(&esp);
+
+boolean wifiConnected = false;
+
+void wifiCb(void* response)
 {
-  Serial.begin(9600);
+  uint32_t status;
+  RESPONSE res(response);
+
+  if (res.getArgc() == 1) {
+    res.popArgs((uint8_t*)&status, 4);
+    if (status == STATION_GOT_IP) {
+      #ifdef DEBUG
+        debugPort.println("WIFI CONNECTED");
+      #endif
+      wifiConnected = true;
+    } else {
+      wifiConnected = false;
+    }
+
+  }
+}
+
+void setup() {
+  Serial.begin(19200);
+ 
+  #ifdef DEBUG
+    debugPort.begin(19200);
+    debugPort.println("INIT");
+  #endif
+  
   lcd.begin(16, 2);
   lcd.home();
   lcd.print("Hello");
+
+  delay(500);
+  esp.enable();
+  delay(500);
+  esp.reset();
+  delay(500);
+  while (!esp.ready());
+
+  #ifdef DEBUG
+    debugPort.println("ARDUINO: setup rest client");
+  #endif
+  if (!rest.begin(DOMAIN)) {
+    #ifdef DEBUG
+      debugPort.println("ARDUINO: failed to setup rest client");
+    #endif
+    while (1);
+  }
+
+  /*setup wifi*/
+  esp.wifiCb.attach(&wifiCb);
+  esp.wifiConnect(SSID, PASSWORD);
+  #ifdef DEBUG
+    debugPort.println("ARDUINO: system started");
+  #endif
 }
 
+char response[5] = "";
 void loop() {
-  Serial.println("Test");
-  lcd.print("World");
-  delay(1000);
+  lcd.print("test");
+  esp.process();
+  if (wifiConnected) {
+    rest.get(PATH);
+    if (rest.getResponse(response, 50) == HTTP_STATUS_OK) {
+      #ifdef DEBUG
+        lcd.print("World");
+      #endif
+    }
+    delay(1000);
+  }
 }
